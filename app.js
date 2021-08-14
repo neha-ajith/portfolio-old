284adbb369fb9361aaf961b70f8fa460f06a9808
Neha Ajith <nehaajith27@gmail.com>
2021-08-14 17:33:34 +0530

    almost there, not responsive yet

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    let hamburger = document.querySelector('.hamburger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+    hamburger.addEventListener('click', function(){
+        mobileNav.classList.add('open');
+    });
+
+    times.addEventListener('click', function(){
+        mobileNav.classList.remove('open');
+    });
+});
