e91566d52dad0e53263f6c43de855b63000b9cf5
Neha Ajith <nehaajith27@gmail.com>
2021-08-15 12:40:24 +0530

    it's fucked up

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
         mobileNav.classList.remove('open');
     });
 });
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "gatco.automated.mail@gmail.com",
+	Password : "gatco.automated.mail@password",
+	To : 'nehaajith27@gmail.com',
+	From : "gatco.automated.mail@gmail.com",
+	Subject : "Mail via your site",
+	Body :  `
+    You have a message from ${document.getElementById("mail-sender-name").value} 
+    <br> Mail id : ${document.getElementById("mail-sender-mail").value} 
+    <br> Message : ${document.getElementById("mail-sender-message").value}
+    <center><button style="background-color:blue">Button</button></center>
+    `,
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
